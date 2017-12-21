3f86426d9ac49b8135c737af41a7822ea47887aa
Arsenii Babitckii <a.babitckii@gmail.com>
2017-12-21 18:39:27 +0200

    Fix js script (now working on firefox)
    Signed-off-by: Arsenii Babitckii <a.babitckii@gmail.com>

--- js/donate_addToCart.js
+++ js/donate_addToCart.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $("form").submit(function() {
+  $("form").submit(function(e) {
+    e.preventDefault();
     var form_data = $(this).serialize();
     $.ajax({
       type: "POST",
